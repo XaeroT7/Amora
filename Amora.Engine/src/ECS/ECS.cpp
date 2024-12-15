2af2b0c47a8c99101aae7cb8bbac7cc9a108b5a6
XaeroT7 <jhorner@hey.com>
2024-12-15 14:15:48 -0500

    Define more registry methods

--- Amora.Engine/src/ECS/ECS.cpp
+++ Amora.Engine/src/ECS/ECS.cpp
@@ -1,16 +1,8 @@
 #include "ECS.h"
-
-Entity::Entity(int id): id(id) {
-}
+#include "../Logger/Logger.h"
 
 bool Entity::operator==(const Entity& other) const {
 	return other.id == id;
-}
-
-System::System() {
-}
-
-System::~System() {
 }
 
 template<typename TComponent>
@@ -26,3 +18,43 @@ void System::remove_entity(Entity entity) {
 		}
 	}
 }
+
+Entity Registry::create_entity() {
+	int entity_id = num_entities++;
+	if (entity_id >= entity_component_signatures.size()) {
+		entity_component_signatures.resize(entity_id + 1);
+	}
+	Entity entity(entity_id);
+	entitiesToBeAdded.insert(entity);
+	Logger::log("Entity created with id = " + std::to_string(entity_id));
+
+	return entity;
+}
+
+void Registry::add_entity_to_system(Entity entity) {
+}
+
+template<typename T, typename ...TArgs>
+void Registry::add_component(Entity entity, TArgs&& ...) {
+
+}
+
+template<typename T>
+void Registry::remove_component(Entity entity) {
+}
+
+template<typename T>
+bool Registry::has_component(Entity entity) const {
+	return false;
+}
+
+template<typename T>
+T& Registry::get_component(Entity entity) const {
+	// TODO: insert return statement here
+}
+
+void Registry::update() {
+	// TODO: Add entities that are waiting to be created
+	// TODO: Remove entities waiting to be destroyed
+}
+
--- Amora.Engine/src/ECS/ECS.h
+++ Amora.Engine/src/ECS/ECS.h
@@ -1,12 +1,24 @@
 #ifndef ECS_H 
 #define ECS_H 
 
-#include <vector>
 #include <bitset>
+#include <vector>
+#include <set>
+#include <typeindex>
+#include <unordered_map>
 
 const unsigned int MAX_COMPONENTS = 32;
 
 typedef std::bitset<MAX_COMPONENTS> Signature;
+
+struct Entity {
+	Entity(int id) : id(id) {};
+
+	int id;
+
+	bool operator==(const Entity& other) const;
+};
+
 
 struct BaseComponent {
 	static int next_id;
@@ -20,28 +32,78 @@ struct Component : public BaseComponent {
 	}
 };
 
-struct Entity {
-	Entity(int id);
 
-	int id;
-
-	bool operator==(const Entity other) const;
-};
-
+//---------------------------------------------------------------
+// SYSTEM 
+//---------------------------------------------------------------
+// Processes entities that contain a specific component signature
+//---------------------------------------------------------------
 struct System {
-	System();
-	~System();
+	System() = default;
+	~System() = default;
 
 	Signature component_signature;
 	std::vector<Entity> entities;
+	
+	void remove_entity(Entity entity);
 
 	template<typename TComponent>
 	void require_component();
-	void remove_entity(Entity entity);
+};
+
+
+//---------------------------------------------------------------
+// REGISTRY
+//---------------------------------------------------------------
+// Manages the creation and destruction of entities, as well as 
+// adding systems and components to entities.
+//---------------------------------------------------------------
+struct BasePool {
+	virtual ~BasePool() = 0 { };
+};
+
+template<typename T>
+struct Pool : public BasePool {
+	std::vector<T> components;
+
+	Pool(int size = 100) {
+		components.resize(size);
+	}
+	~Pool() = default;
 };
 
 struct Registry {
-	// TODO: ...
+	int num_entities;
+	std::set<Entity> entitiesToBeAdded;
+	std::set<Entity> entitiesToBeDestroyed;
+
+	std::vector<BasePool> component_pools;					// [index = component type id]
+	std::vector<Signature> entity_component_signatures;		// [index = entity id]
+	std::unordered_map<std::type_index, System*> systems;
+
+	Entity create_entity();
+
+	void update();
+	void add_entity_to_system(Entity entity);
+
+	template<typename T, typename ...TArgs> 
+	void add_component(Entity entity, TArgs&& ...);
+
+	template<typename T>
+	void remove_component(Entity entity);
+
+	template<typename T>
+	bool has_component(Entity entity) const;
+
+	template<typename T>
+	T& get_component(Entity entity) const;
+
+
+	// TODO:
+	// add_component(Entity entity)
+	// get_component(Entity entity)
+	// add_system()
 };
 
 #endif
+
